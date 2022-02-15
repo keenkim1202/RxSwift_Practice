509afda79bf6ecf0a0f76a47eafd03c4ea3dccd6
KEEN <homurahomu@naver.com>
2022-02-15 17:23:46 +0900

    [subject] BehaviorSubject

--- RxSwift_Practice/Observable_pg.playground/Contents.swift
+++ RxSwift_Practice/Observable_pg.playground/Contents.swift
@@ -1,53 +1,21 @@
 import UIKit
 import RxSwift
 
+// Behavior Subject
 let disposeBag = DisposeBag()
 
-/*
- ## PublishSubject
- - 기본적으로 객체이다.
- - 구독과 이벤트 방출을 둘 다 할 수 있는 객체 이다.
- - 기본값을 줄 필요가 없다. (초기화하지 않아도 됨)
- 
- -> 구독자(subscriber)이자 옵저버(observer) 인 것!
- */
+// 초기값을 가진다.
+let subject = BehaviorSubject(value: "inital value") // 초기값 부여하기
 
-let subject = PublishSubject<String>() // 이 객체는 string 타입의 이벤트만 방출 가능
-
-subject.onNext("issue 1")
-
-// 위의 까지만 하면 구독을 하지 않았기 때문에 아무일도 일어나지 않는다.
+// subscribe가 발생하면, 발생한 시점 이전에 발생한 이벤트 중 가장 최신의 이벤트를 전달받는다.
+subject.onNext("last last issue") // 가장 최신 이벤트만 출력되므로 얘는 출력 안된다.
+subject.onNext("last issue") // 구독 전에 작성을 해도 출력이 된다.
 
 subject.subscribe { event in
   print(event)
 }
 
-subject.onNext("issue 2")
-subject.onNext("issue 3")
+subject.onNext("issue 1")
 
-/*
- 출력
- next(issue 2)
- next(issue 3)
- */
-
-// 모든 이벤트는 구독이 이루어진 후에 일어난다.
-// 그렇기 때문에 구독을 하기 전에 발생한 이벤트는 프린트 되지 않는다.
-
-
-// dispose 한 이후에 작성한 이벤트 issue4는 무시되기 때문에 출력되지 않는다.
-subject.dispose()
-subject.onNext("issue 4")
-
-
-// completed 한 경우 oncompleted 까지만 출력된다.
-// subject.onCompleted()
-// subject.onNext("issue 4")
-
-// dispose 한다면 그 이후의 이벤트는 다 무시된다.
-// subject.dispose()
-// subject.onCompleted()
-// subject.onNext("issue 4")
-
-
-
+// publish 와 다른점 1가지
+// behavior는 값을 하나 방출해야하고 내가 방출하지 않으면 자동으로 지난값을 방출한다.
