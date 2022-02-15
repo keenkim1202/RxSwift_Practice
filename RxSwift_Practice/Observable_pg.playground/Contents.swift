5575238149c809c983c3d1978f192b28c3b08b33
KEEN <homurahomu@naver.com>
2022-02-16 01:18:45 +0900

    [subject] replaysubject

--- RxSwift_Practice/Observable_pg.playground/Contents.swift
+++ RxSwift_Practice/Observable_pg.playground/Contents.swift
@@ -1,21 +1,31 @@
 import UIKit
 import RxSwift
 
-// Behavior Subject
+// Relay Subject
 let disposeBag = DisposeBag()
 
-// 초기값을 가진다.
-let subject = BehaviorSubject(value: "inital value") // 초기값 부여하기
+// subject와 구독자는 동일한 값을 가지고 있고, 버퍼 사이즈 만큼 값을 전달 받을 수 있다.
+// 버퍼 사이즈를 기반으로 이벤트에 replay(답신)을 하는 것이다.
+let subject = ReplaySubject<String>.create(bufferSize: 2)
 
-// subscribe가 발생하면, 발생한 시점 이전에 발생한 이벤트 중 가장 최신의 이벤트를 전달받는다.
-subject.onNext("last last issue") // 가장 최신 이벤트만 출력되므로 얘는 출력 안된다.
-subject.onNext("last issue") // 구독 전에 작성을 해도 출력이 된다.
-
-subject.subscribe { event in
-  print(event)
-}
-
+// 1, 2, 3이 방출되었지만, 버퍼사이즈 만큼의 값을 전달 받을 수 있기 때문에
 subject.onNext("issue 1")
+subject.onNext("issue 2")
+subject.onNext("issue 3")
 
-// publish 와 다른점 1가지
-// behavior는 값을 하나 방출해야하고 내가 방출하지 않으면 자동으로 지난값을 방출한다.
+// 구독했을 때 1은 방출되지 않는다. (실질적으로 값을 넘겨준 갯수는 버퍼의 크기 만큼)
+
+// 구독한 subject의 최근값을 버퍼 사이즈만큼 프린트한다. (2,3)
+subject.subscribe { event in
+  print("first", event)
+}.disposed(by: disposeBag)
+
+// onNext에서 방출한 이벤트들을 출력한다. (4,5,6)
+subject.onNext("issue 4")
+subject.onNext("issue 5")
+subject.onNext("issue 6")
+
+// 새로 구독한 subject의 최근값을 버퍼 사이즈만큼 프린트한다. (5,6)
+subject.subscribe { event in
+  print("second", event)
+}.disposed(by: disposeBag)
