4fab00fb6494147b8ccee7982022b4035c528dde
KEEN <homurahomu@naver.com>
2022-02-16 13:38:09 +0900

    [Variable]

--- RxSwift_Practice/Observable_pg.playground/Contents.swift
+++ RxSwift_Practice/Observable_pg.playground/Contents.swift
@@ -1,31 +1,27 @@
 import UIKit
 import RxSwift
 
-// Relay Subject
+// Variable <- deprecate 될 예정. BehaviorRelay를 사용하도록 하자
 let disposeBag = DisposeBag()
 
-// subject와 구독자는 동일한 값을 가지고 있고, 버퍼 사이즈 만큼 값을 전달 받을 수 있다.
-// 버퍼 사이즈를 기반으로 이벤트에 replay(답신)을 하는 것이다.
-let subject = ReplaySubject<String>.create(bufferSize: 2)
+// behavior subject와 상태를 래핑하고 있다. -> 현재 값을 상태로 가지고 있다.
+// 그리고 해당 프로퍼티를 통해 값에 접근할 수 있다.
+// behavior subject를 사용하한 시점부터, 변수에 어떤 초기값을 넘겨주어야 한다.
 
-// 1, 2, 3이 방출되었지만, 버퍼사이즈 만큼의 값을 전달 받을 수 있기 때문에
-subject.onNext("issue 1")
-subject.onNext("issue 2")
-subject.onNext("issue 3")
+// let varaiable = Variable("init value")
+// varaiable.value = "changed value"
 
-// 구독했을 때 1은 방출되지 않는다. (실질적으로 값을 넘겨준 갯수는 버퍼의 크기 만큼)
+let varaiable = Variable([String]())
+varaiable.value.append("item 1")
 
-// 구독한 subject의 최근값을 버퍼 사이즈만큼 프린트한다. (2,3)
-subject.subscribe { event in
-  print("first", event)
-}.disposed(by: disposeBag)
 
-// onNext에서 방출한 이벤트들을 출력한다. (4,5,6)
-subject.onNext("issue 4")
-subject.onNext("issue 5")
-subject.onNext("issue 6")
+varaiable.asObservable()
+  .subscribe {
+    print($0)
+  }
 
-// 새로 구독한 subject의 최근값을 버퍼 사이즈만큼 프린트한다. (5,6)
-subject.subscribe { event in
-  print("second", event)
-}.disposed(by: disposeBag)
+// variable이 옵저버블로 바뀐 다음부터는, 값이 바뀔 때마다 실행된다.
+// 이것이 좋은 이유는 배열의 값이 바뀔떄 우리는 배열 안에서 값을 추가하고 지워주는 작업을 해야한다.
+varaiable.value.append("item 2")
+
+// complete, error 이벤트가 발생하지 않으며 variable이 해제될 때 자동으로 complete 된다.
