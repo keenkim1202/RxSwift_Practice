4c31489e281814f81eddae4c62b3cdd806095f44
KEEN <homurahomu@naver.com>
2022-02-16 14:50:51 +0900

    [FEAT] CameraFilter 프로젝트 카메라 권한 설정

--- CameraFilter/CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/CameraFilter/PhotosCollectionViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import Photos
 
-class PhotosCollectionViewController: UIViewController {
+class PhotosCollectionViewController: UICollectionViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    populatePhotos()
   }
   
+  private func populatePhotos() {
+    PHPhotoLibrary.requestAuthorization { status in
+      if status == .authorized {
+        // 사진첩 접근 권한이 있는 경우
+        
+      }
+    }
+  }
 }
