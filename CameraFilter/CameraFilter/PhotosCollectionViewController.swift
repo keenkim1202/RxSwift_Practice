8f225cf3bac52bcd7e182dbdc5e5996168f274df
KEEN <homurahomu@naver.com>
2022-02-16 14:57:27 +0900

    [FEAT] 사진첩의 이미지 정보 가저오는 함수 작성

--- CameraFilter/CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/CameraFilter/PhotosCollectionViewController.swift
@@ -10,17 +10,38 @@ import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
   
+  private var images = [PHAsset]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     populatePhotos()
   }
   
   private func populatePhotos() {
-    PHPhotoLibrary.requestAuthorization { status in
-      if status == .authorized {
-        // 사진첩 접근 권한이 있는 경우
+    PHPhotoLibrary.requestAuthorization { [weak self] status in
+      if status == .authorized { // 사진첩 접근 권한이 있는 경우
+        let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
         
+        assets.enumerateObjects { (object, count, stop) in
+          self?.images.append(object)
+        }
+        
+        self?.images.reverse() // 가장 마지막에 선택한 이미지를 사용하기 위해서 reverse
+        // print(self?.images)
       }
     }
+  }
+  
+  override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return self.images.count
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = UICollectionViewCell()
+    return cell
   }
 }
