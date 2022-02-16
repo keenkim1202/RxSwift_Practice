3ebbc17b1b9c6eacda6ad392c8831741151a335d
KEEN <homurahomu@naver.com>
2022-02-16 15:28:47 +0900

    [FEAT] 선택 이미지를 담을 publishSubject 생성 및 선택이미지 담아주기

--- CameraFilter/CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/CameraFilter/PhotosCollectionViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 import Photos
+import RxSwift
 
 class PhotosCollectionViewController: UICollectionViewController {
+  
+  private let selectedPhotoSubject = PublishSubject<UIImage>()
+  var selectedPhoto: Observable<UIImage> {
+    return selectedPhotoSubject.asObservable()
+  }
   
   private var images = [PHAsset]()
   
@@ -55,5 +61,21 @@ class PhotosCollectionViewController: UICollectionViewController {
     }
     
     return cell
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let selectedAsset = self.images[indexPath.item]
+    PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+      guard let info = info else { return }
+      
+      let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+      
+      if !isDegradedImage {
+        if let image = image {
+          self?.selectedPhotoSubject.onNext(image)
+          self?.dismiss(animated: true, completion: nil)
+        }
+      }
+    }
   }
 }
